82eeb4dbb1bf6b78d8c66da8cd184fa216aae243
AuArgen <argenazizbekuulu3@gmail.com>
2024-04-17 16:21:51 +0600

    model klassteacher

--- klassteacher/models.py
+++ klassteacher/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+from klass.models import Klass
+from teacher.models import Teacher
+
+
+class Klasssteacher(models.Model):
+    teacher = models.ForeignKey(Teacher, on_delete=models.CASCADE, verbose_name='Мугалим')
+    klass = models.OneToOneField(Klass, on_delete=models.CASCADE, verbose_name='Класс', unique=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Класс жетечи'
+        verbose_name_plural = 'Класс жетечилер'
+
+    def __str__(self):
+        return f'{self.teacher.user.last_name} {self.teacher.user.first_name} {self.klass.title}'
