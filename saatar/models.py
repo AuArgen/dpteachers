bb4255ecbaf0c382019c1fadd8ca4e5c6c969623
AuArgen <argenazizbekuulu3@gmail.com>
2024-04-17 16:38:35 +0600

    model saatar

--- saatar/models.py
+++ saatar/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Saatar(models.Model):
+    title = models.CharField(max_length=10, unique=True, verbose_name='Аталышы')
+    saat = models.TimeField(verbose_name='Сааты')
+
+    class Meta:
+        verbose_name = 'Саат'
+        verbose_name_plural = 'Сааттар'
+
+    def __str__(self):
+        return self.title
