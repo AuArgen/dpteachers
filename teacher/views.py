a7aef4482fb91ced4b75938e94e9217206c1e406
AuArgen <argenazizbekuulu3@gmail.com>
2024-04-23 00:22:02 +0600

    login page

--- teacher/views.py
+++ teacher/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login
 
-# Create your views here.
+
+@login_required
+def home(request):
+    return render(request, 'base.html')
+
+
+def logins(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        messages.error(request, 'Логин же пароль туура эмес')
+    return render(request, 'login.html')
