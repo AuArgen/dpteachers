93beb87980d8bfa4129c306161f2b7c4402a3244
AuArgen <argenazizbekuulu3@gmail.com>
2024-04-22 23:21:56 +0600

    model news

--- news/models.py
+++ news/models.py
@@ -1,3 +1,23 @@
+import os
+import uuid
+
 from django.db import models
 
-# Create your models here.
+def generate_unique_filename(instance, filename):
+    ext = filename.split('.')[-1]
+    filename = f'{uuid.uuid4()}.{ext}'
+    return os.path.join('media/news/avatars/', filename)
+
+class News(models.Model):
+    doc = models.FileField(upload_to=generate_unique_filename, blank=True, null=True, verbose_name='Документ')
+    image = models.ImageField(upload_to=generate_unique_filename, blank=True, null=True, verbose_name='Cүрөт')
+    title = models.CharField(max_length=100, blank=True, null=True, verbose_name='Тема')
+    description = models.TextField(null=True, blank=True, verbose_name='Кошумча маалымат')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        verbose_name = 'Жаңылык'
+        verbose_name_plural = 'Жаңылыктар'
+
+    def __str__(self):
+        return f'  {self.title} {self.description} {self.image} {self.doc}'
