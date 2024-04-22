b36678639b639dd2fec9fe97f3bdd461d1d6de1b
AuArgen <argenazizbekuulu3@gmail.com>
2024-04-23 00:32:41 +0600

    login page redirect

--- teacher/views.py
+++ teacher/views.py
@@ -3,10 +3,12 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate, login
 
+from children.models import Child
+
 
 @login_required
 def home(request):
-    return render(request, 'base.html')
+    return render(request, 'teacher.html')
 
 
 def logins(request):
@@ -16,6 +18,8 @@ def logins(request):
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
+            if Child.objects.filter(child=user).exists():
+                return redirect('children')
             return redirect('home')
         messages.error(request, 'Логин же пароль туура эмес')
     return render(request, 'login.html')
