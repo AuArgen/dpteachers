76a2965b51c058909b688b5874180d0c339ec7de
AuArgen <argenazizbekuulu3@gmail.com>
2024-04-28 00:07:00 +0600

    home page index

--- children/views.py
+++ children/views.py
@@ -1,6 +1,11 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
+from news.models import News
+
+
 @login_required
-def home (request):
-    return render(request, 'child/index.html')
+def home(request):
+    news = News.objects.all().order_by('-id')
+    context = {'news': news}
+    return render(request, 'child/index.html', context)
