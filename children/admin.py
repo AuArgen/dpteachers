be0f7806f0e9cb5cbef0192ea03b02a666df8245
AuArgen <argenazizbekuulu3@gmail.com>
2024-04-29 09:49:13 +0600

    filter

--- children/admin.py
+++ children/admin.py
@@ -6,6 +6,7 @@ from children.models import Child
 @admin.register(Child)
 class ChildAdmin(admin.ModelAdmin):
     list_display = ('id', 'get_first_name', 'klass', 'phone', 'address')
-
+    list_filter = ('klass', 'address')
+    search_fields = ('klass', 'get_first_name')
     def get_first_name(self, obj):
         return f'{obj.child.last_name} {obj.child.first_name}' if obj.child else ''
