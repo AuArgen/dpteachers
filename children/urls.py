f8f023d6dc8e9e66f2ecfc6ecbde625a0e89f4bf
AuArgen <argenazizbekuulu3@gmail.com>
2024-04-29 08:32:18 +0600

    raspisanie

--- children/urls.py
+++ children/urls.py
@@ -3,4 +3,5 @@ from .views import *
 
 urlpatterns = [
     path('', home, name='children'),
+    path('raspisanie/', raspisanie, name='children-raspisanie'),
 ]
--- children/views.py
+++ children/views.py
@@ -1,7 +1,10 @@
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 
+from children.models import Child
+from klass.models import Klass
 from news.models import News
+from raspisanie.models import Raspisanie
 
 
 @login_required
@@ -9,3 +12,26 @@ def home(request):
     news = News.objects.all().order_by('-id')
     context = {'news': news}
     return render(request, 'child/index.html', context)
+
+
+@login_required
+def raspisanie(request):
+    DAYS_OF_WEEK = [
+        'Понедельник',
+        'Вторник',
+        'Среда',
+        'Четверг',
+        'Пятница',
+        'Суббота',
+        'Воскресенье',
+    ]
+    child = Child.objects.get(child=request.user)
+    raspisanie = Raspisanie.objects.filter(klass=child.klass).order_by('saat__saat_start')
+    ras = [];
+    for i in DAYS_OF_WEEK:
+        ra = []
+        for j in raspisanie.filter(day=i):
+            ra.append(j)
+        ras.append(ra)
+    context = {'raspisanie': ras}
+    return render(request, 'child/raspisanie.html', context)
